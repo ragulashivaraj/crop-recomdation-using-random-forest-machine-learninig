626f9ccd143221b936633b9561b1a5ac21e7260b
ragulashivaraj <ragulashibbu@gmail.com>
2024-06-23 14:26:28 +0530

    Add files via upload

---
+++ CropRecommendation/models/output.py
@@ -0,0 +1,8 @@
+data = RandomForestClassifier(bootstrap=True, ccp_alpha=0.0, class_weight=None,
+                       criterion='gini', max_depth=None, max_features='auto',
+                       max_leaf_nodes=None, max_samples=None,
+                       min_impurity_decrease=0.0, min_impurity_split=None,
+                       min_samples_leaf=1, min_samples_split=2,
+                       min_weight_fraction_leaf=0.0, n_estimators=20,
+                       n_jobs=None, oob_score=False, random_state=0, verbose=0,
+                       warm_start=False)
